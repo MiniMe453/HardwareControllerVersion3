2f348f89d8521d05b716939f7ac33d453edb9549
Trey Ramm <p.f.ramm@gmail.com>
2023-01-31 12:28:52 -0800

    figured out the resetting of the game

--- Assets/Scripts/Managers/GameInitializer.cs
+++ Assets/Scripts/Managers/GameInitializer.cs
@@ -9,6 +9,7 @@ public class GameInitializer : MonoBehaviour
 {
     void Awake()
     {
+        Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
         ArduinoInputDatabase.InitializeDatabase();
         
         // QualitySettings.vSyncCount = 0;  // VSync must be disabled
--- gameReset.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-taskkill /IM asperascp.exe /F
-cd "C:\Program Files (x86)\Aspera\Point-to-Point\bin\"
-Start "" /b asperascp.exe 

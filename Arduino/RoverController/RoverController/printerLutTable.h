ef4643d5c1320b7ae33217cd19bad6b736cbc032
Trey Ramm <p.f.ramm@gmail.com>
2023-01-31 09:17:56 +0100

    updated printer lut table, updated bushes

--- Arduino/RoverController/RoverController/printerLutTable.h
+++ Arduino/RoverController/RoverController/printerLutTable.h
@@ -7,6 +7,7 @@ static const String printer_lines[][10] = {
   {"REACTOR_CORE","No data logs","","","","","","0.0125 uSv","51.2 Gauss","-62.1 C"},
   {"POWER_STATION","03/29 16:29 Setup Complete","03/31 08:49 Remote Start Success","05/15 13:59 Maintnence Complete","07/15 23:01 Anomaly Detected","07/30 17:38 Temperature Raised","07/30 17:40 Critical Core Temp","0.0198 uSv","95.5 Gauss","-61.8 C"},
   {"RON_DAVIS","07/30 16:26 Suit Initiliazed","07/30 16:30 Suit Temp Adjusted","07/30 17:26 Radiation Detected","07/30 17:41 High Radiation","07/30 17:43 Oxygen Tank Ruptured","07/30 18:01 Oxygen Supply Empty","0.0134 uSv","26 Gauss","-61.9 C"},
+  {"UNKNOWN","No data logs","","","","","","0.00001 uSv","201.4 Gauss","20.4 C"},
 };
 
 #endif
